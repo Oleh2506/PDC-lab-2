704ef89bdb60cebbc89dd4e8786b3644332cc0cc
Oleh <basarab.oleh@lll.kpi.ua>
2024-03-12 08:13:07 +0200

    Finished main method of TesterDemo class.

--- src/main/TesterDemo.java
+++ src/main/TesterDemo.java
@@ -2,27 +2,43 @@ package main;
 
 import matrixmultiplier.DefaultMultiplier;
 import matrixmultiplier.FoxMultiplier;
+import matrixmultiplier.StripeMultiplier;
 
 public class TesterDemo {
     public static void main(String[] args) {
-        Matrix matrixA = new Matrix(new int[][]{
-                {1, 5, 3},
-                {2, 3, 5},
-                {1, 7, 4}
-        });
+        int matrixSize = 4;
+        int threadCount = 4;
+        int minValue = -10;
+        int maxValue = 10;
 
-        Matrix matrixB = new Matrix(new int[][]{
-                {1, 2, 3},
-                {5, 2, 8},
-                {3, 6, 1}
-        });
+        Matrix matrixA = new Matrix(Tester.generateRandomMatrix(matrixSize, matrixSize, minValue, maxValue));
+        Matrix matrixB = new Matrix(Tester.generateRandomMatrix(matrixSize, matrixSize, minValue, maxValue));
+
+        System.out.println("Matrix A:");
+        matrixA.print();
+        System.out.println("\nMatrix B:");
+        matrixB.print();
 
         DefaultMultiplier defaultMultiplier = new DefaultMultiplier(matrixA, matrixB);
-        Matrix matrixC = defaultMultiplier.multiply();
-        matrixC.print();
-        System.out.println("----");
-        FoxMultiplier foxMultiplier = new FoxMultiplier(matrixA, matrixB, 2);
-        matrixC = foxMultiplier.multiply();
-        matrixC.print();
+        Matrix defaultMatrixC = defaultMultiplier.multiply();
+        System.out.println("\nMatrix C (C = A x B), sequential:");
+        defaultMatrixC.print();
+
+        StripeMultiplier stripeMultiplier = new StripeMultiplier(matrixA, matrixB, threadCount);
+        Matrix stripeMatrixC = stripeMultiplier.multiply();
+        System.out.println("\nMatrix C (C = A x B), stripe:");
+        stripeMatrixC.print();
+
+        FoxMultiplier foxMultiplier = new FoxMultiplier(matrixA, matrixB, threadCount);
+        Matrix foxMatrixC = foxMultiplier.multiply();
+        System.out.println("\nMatrix C (C = A x B), fox:");
+        foxMatrixC.print();
+
+        if (defaultMatrixC.isEqual(foxMatrixC) && defaultMatrixC.isEqual(stripeMatrixC)) {
+            System.out.println("\nResults are valid.");
+        }
+        else {
+            System.out.println("\nError!");
+        }
     }
 }
